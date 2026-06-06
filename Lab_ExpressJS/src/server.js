1b1ffcd8648348febf14312704f2f65c2cad2c87
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-06-06 15:34:47 +0300

    Add dotenv configuration and example .env file for environment variables

--- Lab_ExpressJS/src/server.js
+++ Lab_ExpressJS/src/server.js
@@ -1,11 +1,11 @@
-
+require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 
-    mongoose.connect("mongodb://localhost:27017/Node_Lab").then(() => {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("Connected to MongoDB");
     }).catch((err) => {
         console.error("Failed to connect to MongoDB", err);
